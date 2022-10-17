2a94c4eeb24109eb18fbabe29576503c5586b6a7
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-10-17 12:28:08 -0400

    create file / comment stuff to clear problem on terminal / create firts part of CallButton

---
+++ Commercial_Controller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Commercial_Controller
+{
+    class Program
+    {
+        static void Main()
+        {
+    CallButton testbouton = new CallButton(2,"up" );
+
+             System.Console.WriteLine(testbouton.ID);
+             System.Console.WriteLine(testbouton.status);
+
+
+            // int scenarioNumber = Int32.Parse(args[0]);
+            // Scenarios scenarios = new Scenarios();
+            // scenarios.run(scenarioNumber);
+        }
+    }
+}
---
+++ Commercial_Controller/callButton.cs
@@ -0,0 +1,14 @@
+namespace Commercial_Controller
+{
+    //Button on a floor or basement to go back to lobby
+    public class CallButton
+    {
+         public int ID = 1;
+         public string status = "status";
+        public CallButton(int _floor, string _direction)
+        { 
+            
+        }
+    }
+}
+
